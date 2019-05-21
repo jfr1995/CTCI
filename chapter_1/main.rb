67f2ca54a82d4e8102d8d7fa2af9abf26bb03f85
Juan Ramirez <juanramirez1776@gmail.com>
2019-05-20 18:58:30 -0700

    power of 2

--- chapter_1/main.rb
+++ chapter_1/main.rb
@@ -140,4 +140,18 @@ end
 
 
   
-  
+  # Write a method that takes in a number and returns true if it is a
+# power of 2. Otherwise, return false.
+#
+# You may want to use the `%` modulo operation. `5 % 2` returns the
+# remainder when dividing 5 by 2; therefore, `5 % 2 == 1`. In the case
+# of `6 % 2`, since 2 evenly divides 6 with no remainder, `6 % 2 == 0`.
+#
+# Difficulty: medium.
+
+def is_power_of_two?(num)
+  if(Math::log(num, 2) % 1 != 0)
+    return false 
+  end 
+  return true
+end
