07e1446cb44757f01954477e7f806482c17aea21
Juan Ramirez <juanramirez1776@gmail.com>
2019-06-09 12:29:19 -0700

    medium in arrays

--- leet/index.js
+++ leet/index.js
@@ -1,7 +1,10 @@
 /**
  * @param {string} s
  * @return {number}
+ * lengthOfLongestSubstring
  */
+
+// correct solution
 
 const isUnique = s => {
   let mySet = new Set();
@@ -33,4 +36,39 @@ var lengthOfLongestSubstring = function(s) {
     }
   }
   return answer;
+};
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ * medium of sorted array
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+  let medium = 0;
+  let merged_array = [];
+  let max =
+    nums1[nums1.length - 1] > nums2[nums2.length - 1]
+      ? nums1[nums1.length - 1]
+      : nums2[nums2.length - 1];
+  for (let i = 0; i <= max; i++) {
+    if (nums1.includes(i) || nums2.includes(i)) {
+      merged_array.push(i);
+    }
+  } // end of first for loop
+  for (
+    let i = 0, j = merged_array.length - 1;
+    i < merged_array.length;
+    i++, j--
+  ) {
+    if (j === i) {
+      medium = merged_array[i];
+      break;
+    }
+
+    if (i > j) {
+      medium = (merged_array[i] + merged_array[j]) / 2;
+    }
+  }
+  return medium;
 };
