e407fc3f88304d603ab3db3998a5951bfc93a848
Juan Ramirez <juanramirez1776@gmail.com>
2019-06-09 17:35:51 -0700

    reverse integer

--- leet/index.js
+++ leet/index.js
@@ -72,3 +72,41 @@ var findMedianSortedArrays = function(nums1, nums2) {
   }
   return medium;
 };
+
+/* REVERSE OF INT 
+
+check for overflow:
+if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+
+
+Explanation:
+This is dependent on the value of INT_MAX.
+
+Consider:
+
+INT_MAX / 10 + INT_MAX % 10 = INT_MAX
+
+because INT_MAX / 10 will take floor of the division.
+
+So if rev == INT_MAX / 10, then pop must be > INT_MAX % 10 for rev + pop > INT_MAX
+
+INT_MAX is 2147483647 and 2147483647 % 10 is 7.
+
+*/
+
+var reverse = function(x) {
+  var reversed = 0;
+  while (x != 0) {
+    reversed *= 10;
+
+    reversed += x % 10;
+
+    x -= x % 10;
+
+    x /= 10;
+  }
+  if (reversed > 2147483647 || reversed < -2147483648) {
+    return 0;
+  } else return reversed;
+};
